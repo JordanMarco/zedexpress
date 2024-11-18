d08f21f729b8e1ab45f1bc7e4acea5842c7f2b40
jordanMarco <fonwotagne@gmail.com>
2024-11-18 16:44:24 +0100

    initialisation du projet zedexpress

---
+++ api/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Models\AccountType;
+use App\Models\Enums\LanguageEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('cni')->nullable();
+            $table->string('phone');
+            $table->string('country');
+            $table->string('address')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('email')->unique();
+            $table->string('login')->unique();
+            $table->string('language_code')->default(LanguageEnum::FR->value);
+            $table->foreignIdFor(AccountType::class, 'account_id')->constrained()->noActionOnDelete();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
