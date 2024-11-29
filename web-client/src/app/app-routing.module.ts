e1f6f1a64698c1bb80b798e6fed0fb994646f8a2
JordanMarco <fonwotagne@gmail.com>
2024-11-29 03:37:32 +0300

    Writing guest guard

--- web-client/src/app/app-routing.module.ts
+++ web-client/src/app/app-routing.module.ts
@@ -3,10 +3,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FullLayoutComponent } from './shared/layout-components/full-layout/full-layout.component';
 import { content } from './shared/routes/fullroutes';
-import { COUNTENT_ROUTES } from './shared/routes/custom-routes';
-import { CustomLayoutComponent } from './shared/layout-components/custom-layout/custom-layout.component';
-import { ContentLayoutComponent } from './shared/layout-components/content-layout/content-layout.component';
-import { Pages } from './shared/routes/content-routes';
+;
+import { guestGuard } from './authentication/guards/guest.guard';
 import { loginGuard } from './authentication/guards/login.guards';
 
 const routes: Routes = [
@@ -14,6 +12,7 @@ const routes: Routes = [
     path: '',
     component: FullLayoutComponent,
     children: content,
+    canActivate: [loginGuard()]
   },
   {
     path: 'auth/login',
@@ -21,6 +20,7 @@ const routes: Routes = [
       import('./authentication/authentication.module').then(
         (m) => m.AuthenticationModule
       ),
+    canActivate: [guestGuard()]
   }
 ];
 
--- web-client/src/app/authentication/guards/guest.guard.ts
+++ web-client/src/app/authentication/guards/guest.guard.ts
@@ -3,21 +3,23 @@ import {
   CanActivateFn,
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
+  Router,
 } from '@angular/router';
 import { SessionService } from 'src/app/shared/services/session-service';
 import { Observable } from 'rxjs';
 import { CustomNavigationService } from 'src/app/shared/services/custom-navigation.service';
 
-export function loginGuard(): CanActivateFn {
+export function guestGuard(): CanActivateFn {
   return (
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean => {
     const authService: SessionService = inject(SessionService);
     const navigationService: CustomNavigationService = inject(CustomNavigationService);
-    if(!authService.isLoggedIn())
-      navigationService.goTo('auth/login')
-    return authService.isLoggedIn();
+
+    if(authService.isLoggedIn())
+      navigationService.goTo('')
+    return !authService.isLoggedIn();
     
   };
 }
