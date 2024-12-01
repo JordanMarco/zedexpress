eb8e25d607d5d6524b07337028a6e262516e6149
JordanMarco <fonwotagne@gmail.com>
2024-12-01 05:50:18 +0300

    Create and configure  routes for different page

--- web-client/src/app/components/incidents/incidents-routing.module.ts
+++ web-client/src/app/components/incidents/incidents-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IncidentsComponent } from './incidents.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'incidents',
+    children: [
+      { path: '', component: IncidentsComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
