88c3c99bd0605cc53933a65951ffd13ba763fe36
jordanMarco <fonwotagne@gmail.com>
2024-12-01 17:14:19 +0100

    supression des données

--- web-client/src/environments/environment.ts
+++ web-client/src/environments/environment.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-const basePath = 'https://8f05-129-0-60-26.ngrok-free.app';
+const basePath = 'http://127.0.0.1:8000';
 export const environment = {
   production: false,
   basePath,
