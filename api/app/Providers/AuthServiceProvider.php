0a3151147f1a3425cab8275ed71497f3f8938cc8
jordanMarco <fonwotagne@gmail.com>
2024-12-03 08:21:28 +0100

    mise a jour du user controller

--- api/app/Providers/AuthServiceProvider.php
+++ api/app/Providers/AuthServiceProvider.php
@@ -6,6 +6,7 @@ namespace App\Providers;
 
 use App\Models\Enums\AccountTypeEnum;
 use App\Models\User;
+use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -17,7 +18,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
+        // User::class => UserPolicy::class
     ];
 
     /**
