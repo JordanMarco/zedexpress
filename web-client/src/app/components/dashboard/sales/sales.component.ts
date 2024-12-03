8ce86b44f68d6a3f614aa686b25f3c2e81046caa
jordanMarco <fonwotagne@gmail.com>
2024-12-03 19:18:11 +0100

    update dashboard

--- web-client/src/app/components/dashboard/sales/sales.component.ts
+++ web-client/src/app/components/dashboard/sales/sales.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountCode } from 'src/app/shared/enums/enums';
 import { Dashboard } from 'src/app/shared/models/dashboard.model';
 import { DashboardService } from 'src/app/shared/rest-services/dashboard.service';
+import { SessionService } from 'src/app/shared/services/session-service';
 
 @Component({
   selector: 'app-sales',
@@ -10,9 +12,12 @@ import { DashboardService } from 'src/app/shared/rest-services/dashboard.service
 export class SalesComponent implements OnInit{
   isLoading = false;
   result!: Dashboard;
+  showcards = false;
   constructor(
     private dashboardService: DashboardService,
+    private sessionService: SessionService,
   ) {
+    this.showcards = this.sessionService.hasAccounType(AccountCode.CLIENT);
   }
   ngOnInit() {
     this.loadClients();
