202193f21c159b143d1bc07127e5fc810d9ad356
jordanMarco <fonwotagne@gmail.com>
2025-06-02 13:21:13 -0400

    clean code on invoice preview

--- web-client/src/app/components/packages/components/invoice-preview/invoice-preview.component.ts
+++ web-client/src/app/components/packages/components/invoice-preview/invoice-preview.component.ts
@@ -12,20 +12,32 @@ import { ParcelService } from 'src/app/shared/rest-services/parcel.service';
 })
 export class InvoicePreviewComponent {
   parcel!: IColis;
-
+  adress: string = '';
   constructor(
     private route: ActivatedRoute,
     private parcelService: ColisService
-  ) {}
+  ) { }
 
   ngOnInit() {
     const id = this.route.snapshot.params['id'];
     this.parcelService.getParcel(id).subscribe(parcel => {
       this.parcel = parcel;
+      if (this.parcel.country == 'Belgique') {
+        this.adress = "Avenue Louise 231, Bruxelles";
+      } else if (this.parcel.country == 'France') {
+        this.adress = "45 Rue des Lilas, Lyon";
+      } else {
+        this.adress = "Rue 1234, Quartier Bastos, Yaoundé";
+      }
     });
   }
 
   printInvoice() {
     window.print();
   }
+
+  round(amount: number) {
+    return amount === 0 ? 0 : Math.round(amount * 100) / 100;
+  }
+
 }
