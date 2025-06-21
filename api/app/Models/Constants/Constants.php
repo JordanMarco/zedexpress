19e4c0c2f0f6386e1258df356be9502d53154889
jordanMarco <fonwotagne@gmail.com>
2025-06-21 14:18:50 -0400

    duplicate agences

--- api/app/Models/Constants/Constants.php
+++ api/app/Models/Constants/Constants.php
@@ -3,5 +3,5 @@
 namespace App\Models\Constants;
 
 class Constants {
-    const COUNTRIES = ['Belgique', 'France', 'Cameroun'];
+    const COUNTRIES = ['Belgique 1', 'Belgique 2', 'France 1', 'France 2', 'Cameroun 1', 'Cameroun 2'];
 }
